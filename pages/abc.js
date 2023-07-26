f83d8d1b132299edf23126b06bf503e1a2ab1bf3
Ayezah Maryam <ayeza.maryam123@gmail.com>
2023-07-26 19:03:45 +0500

    api-task

--- pages/abc.js
+++ pages/abc.js
@@ -6,7 +6,7 @@ const Abc = () => {
 const [dataStore, setDataStore] = useState(null)
 
 const getData = ()=>{
-    return fetch('https://random-data-api.com/api/users/random_user?size=10').then(
+    return fetch('https://random-data-api.com/api/users/random_user?size=9').then(
         (completeResponse)=>completeResponse.json()
     )
 }
@@ -14,6 +14,9 @@ const getData = ()=>{
 const setData =() =>{
     getData().then((data)=>{
         setDataStore(data)
+        for(let i=0;i<data.length;i++){
+            console.log(data[i])
+        }
     })
 }
 
@@ -24,7 +27,7 @@ useEffect(
 
 return <div>
 
-    <nav className="flex justify-evenly h-16 min-w-[750px] max-w-full items-center bg-slate-50">
+    <nav className="flex justify-evenly h-16 min-w-[750px] max-w-full items-center bg-blue-500 text-white">
         <div className="text-bold text-[36px]">Users</div>
         <div className="w-96">
         <div className="items-center rounded-full bg-white hover:drop-shadow-lg 
@@ -44,20 +47,25 @@ return <div>
     lg:mx-6 xl:mx-24 2xl:mx-28 lg:grid-cols-3 
     2xl:grid-cols-4 my-16">
 
-        {dataStore?.map(({avatar, first_name, last_name, email,id})=>{
-        return <div className="flex my-2 mx-1">
+        {dataStore?.map(({avatar, first_name, last_name, employment,id,gender,subscription})=>{
+        return <div className="flex my-2 mx-1" key={id}>
             <div className="flex bg-slate-50  justify-start
             h-50 min-w-[310px] w-[380px] rounded-2xl space-x-2 p-4 px-5
-            hover:bg-blue-50 hover:scale-100 hover:border-2 
+            hover:bg-blue-50 hover:scale-105 hover:border-2 
             hover:cursor-pointer hover:shadow-lg 
             border border-gray-600 hover:border-blue-300"
-        key={id}>
-                <div className="flex justify-start h-[110px] w-28 rounded-full border overflow-hidden bg-white"><img  src={avatar} /></div>
-                    <div className="flex flex-col pr-2 h-50 justify-center">
-                        <div className="font-bold text-base"> {first_name + " " + last_name}</div>
-                        <div className="text-sm"><span className="font-bold">ID :</span> {id}</div>
-                         <hr/>
+        >
+                <div className="flex justify-start h-28 w-44 rounded-full border overflow-hidden bg-white"><img  src={avatar} /></div>
+                <div className="flex flex-col pr-2 h-50 justify-center w-full">
+                    <div className="font-bold text-base"> {first_name + " " + last_name}</div>
+                    <div className="text-sm">{employment['title']}</div>
+                    <hr/>
+                    <div className="grid grid-cols-2 pt-1" >
+                        <button className="text-xs border rounded-full m-1 hover:border-black border-yellow-600 hover:bg-yellow-500 hover:text-white bg-yellow-200 text-black">{gender}</button>
+                        <button className="text-xs border rounded-full m-1 hover:border-black border-green-600 hover:bg-green-500 hover:text-white bg-green-200 text-black">{subscription['plan']}</button>
                     </div>
+                    <button className="text-xs border rounded-full m-1 hover:border-black border-red-600 hover:bg-red-500 hover:text-white bg-red-200 text-black">{employment['key_skill']}</button>
+                </div>
                 </div>
     </div>
 })}
